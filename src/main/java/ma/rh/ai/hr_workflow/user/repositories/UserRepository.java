9b126077e88a6dac69e4e16401d6f0e6ae5e93b8
ennaciri <azizennaciri0@gmail.com>
2026-02-08 17:41:09 +0100

    Add methods on UserRepository

--- src/main/java/ma/rh/ai/hr_workflow/user/repositories/UserRepository.java
+++ src/main/java/ma/rh/ai/hr_workflow/user/repositories/UserRepository.java
@@ -1,9 +1,16 @@
 package ma.rh.ai.hr_workflow.user.repositories;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import ma.rh.ai.hr_workflow.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    boolean existsByEmail(@Email @NotBlank String email);
+
+    Optional<User> findByEmail(@Email @NotBlank String email);
 }
