5ea5c712b03e4586f02db00a3cfa3e065d9bd867
ennaciri <azizennaciri0@gmail.com>
2026-02-08 19:43:36 +0100

    Add UserController

---
+++ src/main/java/ma/rh/ai/hr_workflow/user/controllers/UserController.java
@@ -0,0 +1,59 @@
+package ma.rh.ai.hr_workflow.user.controllers;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import ma.rh.ai.hr_workflow.user.DTOs.CreateUserDTO;
+import ma.rh.ai.hr_workflow.user.DTOs.LoginRequestDTO;
+import ma.rh.ai.hr_workflow.user.DTOs.LoginResponseDTO;
+import ma.rh.ai.hr_workflow.user.DTOs.UserResponseDTO;
+import ma.rh.ai.hr_workflow.user.service.IUserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    private final IUserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> registerUser(@Valid @RequestBody CreateUserDTO createUserDTO){
+        UserResponseDTO userResponseDTO = userService.register(createUserDTO);
+        return new ResponseEntity<>(userResponseDTO, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> loginUser(@Valid @RequestBody LoginRequestDTO dto){
+        LoginResponseDTO login = userService.login(dto);
+        return ResponseEntity.ok(login);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> updateUser(@PathVariable @Parameter(description = "User ID")Long id
+            ,@Valid @RequestBody CreateUserDTO createUserDTO){
+        UserResponseDTO userResponseDTO = userService.updateUser(id,createUserDTO);
+        return ResponseEntity.ok(userResponseDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> getUserId(@PathVariable @Parameter(description = "User ID")Long id){
+        UserResponseDTO userResponseDTO = userService.getUserById(id);
+        return ResponseEntity.ok(userResponseDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers(){
+        List<UserResponseDTO> userResponseDTO = userService.getAllUsers();
+        return ResponseEntity.ok(userResponseDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> disableUser(@PathVariable @Parameter(description = "User ID")Long id){
+        userService.disableUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
