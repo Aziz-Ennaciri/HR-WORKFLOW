2bf432f471c89ed086d28c26187721816112e183
ennaciri <azizennaciri0@gmail.com>
2026-02-08 17:41:39 +0100

    just modify name of CreateUserDTO

--- src/main/java/ma/rh/ai/hr_workflow/user/DTOs/CreateUserDTO.java
+++ src/main/java/ma/rh/ai/hr_workflow/user/DTOs/CreateUserDTO.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CreateUserDTIO {
+public class CreateUserDTO {
     @Email
     @NotBlank
     private String email;
