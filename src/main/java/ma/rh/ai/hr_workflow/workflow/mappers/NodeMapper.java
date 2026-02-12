82a38f1e02a8bf8e2629c130ac33db0710469701
ennaciri <azizennaciri0@gmail.com>
2026-02-12 22:55:30 +0100

    Modify NodeMapper for type of dto and add NodeType

--- src/main/java/ma/rh/ai/hr_workflow/workflow/mappers/NodeMapper.java
+++ src/main/java/ma/rh/ai/hr_workflow/workflow/mappers/NodeMapper.java
@@ -3,6 +3,7 @@ package ma.rh.ai.hr_workflow.workflow.mappers;
 import ma.rh.ai.hr_workflow.workflow.DTOs.CreateNodeDTO;
 import ma.rh.ai.hr_workflow.workflow.DTOs.NodeResponseDTO;
 import ma.rh.ai.hr_workflow.workflow.model.Node;
+import ma.rh.ai.hr_workflow.workflow.model.NodeType;
 import ma.rh.ai.hr_workflow.workflow.model.Workflow;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -12,7 +13,7 @@ public interface NodeMapper {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "workflow", source = "workflow")
-    @Mapping(target = "type", source = "type")
+    @Mapping(target = "type", source = "dto.type")
     @Mapping(target = "orderIndex", source = "dto.order")
     @Mapping(target = "configJson", source = "dto.configJson")
     Node toEntity(CreateNodeDTO dto, Workflow workflow);
@@ -20,4 +21,8 @@ public interface NodeMapper {
     @Mapping(target = "type", expression = "java(node.getType().name())")
     @Mapping(target = "order", source = "orderIndex")
     NodeResponseDTO toResponseDTO(Node node);
+
+    default NodeType map(String type) {
+        return NodeType.valueOf(type);
+    }
 }
